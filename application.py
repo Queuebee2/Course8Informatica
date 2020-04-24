49798e4d643a1833843d2da30141114849e593a1
Milain Lambers <milain.lambers@gmail.com>
2020-04-24 19:24:21 +0200

    try without wsgi

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from Course8Informatica import create_app
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+# Web Server Gateway Interface
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from Course8Informatica import create_app
-
-# Web Server Gateway Interface
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
