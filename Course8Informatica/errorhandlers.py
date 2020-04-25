5241d6887479dd2cb342aac105830f96fe9aaa88
Milain Lambers <milain.lambers@gmail.com>
2020-04-25 17:08:34 +0200

    add module import test page

--- Course8Informatica/errorhandlers.py
+++ Course8Informatica/errorhandlers.py
@@ -3,12 +3,13 @@ from flask import render_template
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
+    # Todo turn this into a reroute through routes
     return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def server_error(e):
+    # Todo turn this into a reroute through routes
     return render_template('errors/500.html', error_message=e.description), 500
 
 
--- Course8Informatica/routes.py
+++ Course8Informatica/routes.py
@@ -15,10 +15,24 @@ def home():
 @app.route('/database')
 def database_test():
     """database test page."""
-    return render_template('home.html',
+    return render_template('database_test.html',
                            title="database page",
                            description="This is the database")
 
 @app.route('/crash_the_server')
 def server_error_test():
     abort(500, "success")
+
+
+@app.route('/test_module_import')
+def module_import_test():
+
+    # import something
+    from mysql import connector
+    if connector:
+        module_imported = True
+    else:
+        module_imported = False
+    return render_template('testing/module_import_test.html',
+                           title="testing module imports",
+                           module_output=module_imported)
