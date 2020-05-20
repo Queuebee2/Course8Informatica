9d801e4b56bfb3840eb166bff059b3418cc2da67
Bart Jolink <bartjolink1@hotmail.com>
2020-05-20 12:32:32 +0200

    fixed checkboxes. Change template

--- Course8Informatica/routes.py
+++ Course8Informatica/routes.py
@@ -1,7 +1,7 @@
 """Route declaration."""
 from flask import current_app as app
 from flask import render_template
-from flask import request, Markup, Response
+from flask import request, Response, session
 from Course8Informatica import pubmedsearchtool as ps
 from Course8Informatica import gene_retriever as gr
 
@@ -29,58 +29,44 @@ def database():
 def search_test():
     "Search page"
     # search_term = '((variant [tiab] OR variants [tiab] OR mutation [tiab] OR mutations [tiab] OR substitutions [tiab] OR substitution [tiab] ) AND ("loss of function" [tiab] OR "loss-of-function" [tiab] OR "haplo-insufficiency" [tiab] OR haploinsufficiency [tiab] OR "bi-allelic" [tiab] OR "biallelic" [tiab] OR recessive [tiab] OR homozygous [tiab] OR heterozygous [tiab] OR "de novo" [tiab] OR dominant [tiab] OR " X-linked" [tiab]) AND ("intellectual" [tiab] OR "mental retardation" [tiab] OR "cognitive" [tiab] OR "developmental" [tiab] OR "neurodevelopmental" [tiab]) AND “last 2 years”[dp] AND KDM3B) '
-    collapsibles = ""
     search_term = request.form.get("search", "")
     marked = request.form.get("mark", "")
     export = request.form.get("export", "")
 
-    print(request.form.getlist("checkbox"))
-
-    if 'results' not in locals():
-        results = ""
+    if 'collapsible_data' not in locals():
+        collapsible_data = ""
 
     if request.method == 'POST':
         if export == 'Export data':
-            print(request.form.get("checkboxtest"))
             print(request.form.getlist("checkbox"))
-            print("Export data")
             content = ""
-            for i in range(10):
-                checkbox = "checkbox{}".format(i)
-                print(checkbox)
-                print(request.form.get(checkbox, ""))
+            for i in range(len(collapsible_data)):
                 if request.form.get("checkbox{}".format(i), "") == "on":
+                    print(i)
                     try:
-                        print(i)
                         content += "{}".format(i)
-                        # content += results[i]["TI"]
+                        content += collapsible_data[i][0]
                     except KeyError:
                         pass
-            print(content)
             return Response(
                 content,
                 mimetype="text/csv",
                 headers={"Content-disposition":
                              "attachment; filename=db.csv"})
 
-    if marked == 'Mark':
-        print("Mark")
-        for i in range(10):
-            if request.form.get("checkbox" + str(i), "") == "on":
-                print(i)
-        return render_template('search.html',
-                               description=Markup(collapsibles))
+        if marked == 'Mark':
+            print("Mark")
+            for i in range(10):
+                if request.form.get("checkbox" + str(i), "") == "on":
+                    print(i)
 
-    if export != 'Export data' and marked != 'Mark':
-        print("asdfasdfasdf")
-        if search_term != "":
-            results = ps.run_querry(search_term, 'abstract')
-            collapsibles = ps.create_collapsible(results)
-            table = ps.create_table(results)
-            collapsibles = gr.mark_genes(collapsibles)
-        # return render_template('search.html')
+    if search_term != "":
+        results = ps.run_querry(search_term, 'abstract')
+        collapsible_data = ps.create_collapsible(results)
+        table = ps.create_table(results)
+        # collapsibles = gr.mark_genes(collapsibles)
     return render_template('search.html',
-                               description=Markup(collapsibles))
+                               results=collapsible_data)
 
 @app.route('/crash_the_server')
 def server_error_test():
