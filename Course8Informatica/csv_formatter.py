69ef4d0ce0397c62a21e9ea33b10567ed07e76af
Milain Lambers <milain.l.ambers@gmail.com>
2020-06-08 13:17:32 +0200

    docstrings for csv_formatter.py

--- Course8Informatica/csv_formatter.py
+++ Course8Informatica/csv_formatter.py
@@ -1,17 +1,28 @@
 import re
 
+def format_csv_data(PMID, genes, title,delim=';'):
+    """formats data into csv format
 
-def format_csv_data(PMID, genes, title):
+    Args:
+        - PMID (str) : pubmed id
+        - genes (dict) : mapping of genes to a number 
+        - title(markupsafe.Markup)
+        - delim(str) : delimiter
+    Returns:
+        - data (str) : csv-ready formatted data string.
+
+    Todo:
+        1. verify genes param description
+    """
     title = re.sub(r'<span.*<b>|</b></span>', '', title)
     genes = list(genes.keys())
     genedata = ""
     for i in range(len(genes)):
-        print(genes)
         if i < (len(genes) - 1):
             genedata += genes[i] + ","
         else:
             genedata += genes[i]
 
-    data = PMID + ';' + genedata + ';' + title
+    data = f'{PMID}{delim}{genedata}{delim}{title}'
 
     return data
