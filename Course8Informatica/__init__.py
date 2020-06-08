052f8f562f09f87eb65d13df10621e12d975a6f8
Milain Lambers <milain.l.ambers@gmail.com>
2020-06-08 13:24:21 +0200

    remove unused function and imports in __init__.py

--- Course8Informatica/__init__.py
+++ Course8Informatica/__init__.py
@@ -1,21 +1,11 @@
 from flask import Flask
-from .database_clasess import  EnsembleDbHandler
-
-ensemble_db = EnsembleDbHandler()
-
-def create_app():
-    """Construct the core Course8Informatica."""
 
 def app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    ensemble_db.init_app(app)
-
-
     with app.app_context():
         from . import routes, errorhandlers
-
 
         return app
